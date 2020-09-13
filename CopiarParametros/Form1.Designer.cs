2680236d243c6b7172da317ed992b4b00aaa156b
Allan Rodrigo Motroni <allan.motroni@embraport.net>
2020-09-13 10:54:23 -0300

    Add project files.

---
+++ CopiarParametros/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace CopiarParametros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnSalvarXML = new System.Windows.Forms.Button();
+            this.chkTopMost = new System.Windows.Forms.CheckBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.labelValor = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.lstObjetos = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ToolStripMenuItemValor = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusTotal = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.btnAlterar);
+            this.groupBox1.Controls.Add(this.btnSalvarXML);
+            this.groupBox1.Controls.Add(this.chkTopMost);
+            this.groupBox1.Controls.Add(this.txtValor);
+            this.groupBox1.Controls.Add(this.labelValor);
+            this.groupBox1.Controls.Add(this.txtDescricao);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnIncluir);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 112);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpar.Location = new System.Drawing.Point(287, 76);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(57, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAlterar.Location = new System.Drawing.Point(224, 76);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(57, 23);
+            this.btnAlterar.TabIndex = 6;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnSalvarXML
+            // 
+            this.btnSalvarXML.Location = new System.Drawing.Point(83, 76);
+            this.btnSalvarXML.Name = "btnSalvarXML";
+            this.btnSalvarXML.Size = new System.Drawing.Size(72, 23);
+            this.btnSalvarXML.TabIndex = 5;
+            this.btnSalvarXML.Text = "Salvar XML";
+            this.btnSalvarXML.UseVisualStyleBackColor = true;
+            this.btnSalvarXML.Click += new System.EventHandler(this.btnSalvarXML_Click);
+            // 
+            // chkTopMost
+            // 
+            this.chkTopMost.AutoSize = true;
+            this.chkTopMost.Location = new System.Drawing.Point(9, 80);
+            this.chkTopMost.Name = "chkTopMost";
+            this.chkTopMost.Size = new System.Drawing.Size(68, 17);
+            this.chkTopMost.TabIndex = 4;
+            this.chkTopMost.Text = "TopMost";
+            this.chkTopMost.UseVisualStyleBackColor = true;
+            this.chkTopMost.CheckedChanged += new System.EventHandler(this.chkTopMost_CheckedChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtValor.Location = new System.Drawing.Point(70, 48);
+            this.txtValor.MaxLength = 5000;
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(274, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // labelValor
+            // 
+            this.labelValor.AutoSize = true;
+            this.labelValor.Location = new System.Drawing.Point(6, 51);
+            this.labelValor.Name = "labelValor";
+            this.labelValor.Size = new System.Drawing.Size(34, 13);
+            this.labelValor.TabIndex = 3;
+            this.labelValor.Text = "Valor:";
+            this.labelValor.DoubleClick += new System.EventHandler(this.labelValor_DoubleClick);
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDescricao.Location = new System.Drawing.Point(70, 23);
+            this.txtDescricao.MaxLength = 50;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(274, 20);
+            this.txtDescricao.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Descrição:";
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIncluir.Location = new System.Drawing.Point(161, 76);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(57, 23);
+            this.btnIncluir.TabIndex = 2;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // lstObjetos
+            // 
+            this.lstObjetos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstObjetos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstObjetos.ContextMenuStrip = this.contextMenuStrip1;
+            this.lstObjetos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstObjetos.FormattingEnabled = true;
+            this.lstObjetos.ItemHeight = 20;
+            this.lstObjetos.Location = new System.Drawing.Point(12, 130);
+            this.lstObjetos.Name = "lstObjetos";
+            this.lstObjetos.Size = new System.Drawing.Size(350, 242);
+            this.lstObjetos.TabIndex = 1;
+            this.lstObjetos.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lstObjetos_MouseClick);
+            this.lstObjetos.DoubleClick += new System.EventHandler(this.lstObjetos_DoubleClick);
+            this.lstObjetos.KeyUp += new System.Windows.Forms.KeyEventHandler(this.lstObjetos_KeyUp);
+            this.lstObjetos.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lstObjetos_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemValor});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(43, 26);
+            // 
+            // ToolStripMenuItemValor
+            // 
+            this.ToolStripMenuItemValor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ToolStripMenuItemValor.Name = "ToolStripMenuItemValor";
+            this.ToolStripMenuItemValor.Size = new System.Drawing.Size(42, 22);
+            this.ToolStripMenuItemValor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusTotal});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 372);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(374, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(34, 17);
+            this.toolStripStatusLabel1.Text = "Total";
+            // 
+            // toolStripStatusTotal
+            // 
+            this.toolStripStatusTotal.Name = "toolStripStatusTotal";
+            this.toolStripStatusTotal.Size = new System.Drawing.Size(0, 17);
+            this.toolStripStatusTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 394);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lstObjetos);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Copiar Parâmetros";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label labelValor;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.ListBox lstObjetos;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusTotal;
+        private System.Windows.Forms.CheckBox chkTopMost;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemValor;
+        private System.Windows.Forms.Button btnSalvarXML;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnAlterar;
+    }
+}
+
---
+++ CopiarParametros/Form1.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CopiarParametros
+{
+    public partial class Form1 : Form
+    {
+
+        private const string ARQUIVO_XML = "CopiarParametros.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                txtDescricao.KeyPress += TextBox_KeyPress;
+                txtValor.KeyPress += TextBox_KeyPress;
+                txtValor.Visible = false;
+
+                AbrirXML();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void AbrirXML()
+        {
+            try
+            {
+                using (DataSet dsXml = new DataSet())
+                {
+                    dsXml.ReadXml(ARQUIVO_XML);
+                    if (dsXml.Tables.Count > 0)
+                    {
+                        CarregarObjetosXML(dsXml.Tables[0]);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void CarregarObjetosXML(DataTable dt)
+        {
+            List<Objeto> lista = dt.AsEnumerable().Select(p => new Objeto()
+            {
+                Descricao = p.Field<string>("Descricao"),
+                Valor = p.Field<string>("Valor")
+            }).ToList();
+
+            lstObjetos.Items.Clear();
+
+            foreach (Objeto objeto in lista)
+            {
+                IncluirNaLista(objeto);
+            }
+        }
+
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(e.KeyChar) == 13)
+                {
+                    if (((TextBox)sender).Name.ToUpper() == "TXTDESCRICAO")
+                    {
+                        txtValor.Focus();
+                    }
+                    else if (((TextBox)sender).Name.ToUpper() == "TXTVALOR")
+                    {
+                        btnIncluir.PerformClick();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Incluir();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Incluir()
+        {
+            try
+            {
+                var objeto = Validar();
+                Cadastrar(objeto);
+                Limpar();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void lstObjetos_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstObjetos.SelectedIndex > -1)
+                {
+                    var objeto = (Objeto)lstObjetos.SelectedItem;
+                    Clipboard.SetText(objeto.Valor);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Cadastrar(Objeto objeto)
+        {
+            try
+            {
+                IncluirNaLista(objeto);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void GravarArquivo()
+        {
+            try
+            {
+                List<Objeto> objetos = ObterObjetos();
+                string caminho = ARQUIVO_XML;
+
+                using (XmlTextWriter w = new XmlTextWriter(caminho, Encoding.Default))
+                {
+                    w.WriteStartDocument(true);
+                    w.Formatting = Formatting.Indented;
+                    w.Indentation = 2;
+                    w.WriteStartElement("Objetos");
+
+                    foreach (Objeto objeto in objetos)
+                    {
+                        w.WriteStartElement("Objeto");
+
+                        w.WriteStartElement("Descricao");
+                        w.WriteString(objeto.Descricao);
+                        w.WriteEndElement();
+
+                        w.WriteStartElement("Valor");
+                        w.WriteString(objeto.Valor);
+                        w.WriteEndElement();
+
+                        w.WriteEndElement();
+                    }
+                    w.WriteEndDocument();
+                    w.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void IncluirNaLista(Objeto objeto)
+        {
+            try
+            {
+                lstObjetos.ValueMember = "Valor";
+                lstObjetos.DisplayMember = "Descricao";
+                lstObjetos.Items.Add(objeto);
+                AtualizarTotal();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void AtualizarTotal()
+        {
+            try
+            {
+                toolStripStatusTotal.Text = lstObjetos.Items.Count.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private Objeto Validar()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtDescricao.Text))
+                    throw new Exception("Favor preencher o campo Descrição!");
+
+                if (string.IsNullOrWhiteSpace(txtValor.Text))
+                    throw new Exception("Favor preencher o campo Valor!");
+
+                return new Objeto()
+                {
+                    Descricao = txtDescricao.Text.Trim(),
+                    Valor = txtValor.Text.Trim()
+                };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void Limpar()
+        {
+            try
+            {
+                txtDescricao.Clear();
+                txtValor.Clear();
+                txtDescricao.Focus();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void chkTopMost_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = chkTopMost.Checked;
+        }
+
+        private void lstObjetos_MouseUp(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Right && lstObjetos.SelectedIndex > -1)
+                {
+                    var objeto = (Objeto)lstObjetos.SelectedItem;
+                    ToolStripMenuItemValor.Text = objeto.Valor;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnSalvarXML_Click(object sender, EventArgs e)
+        {
+            try
+            {                
+                GravarArquivo();
+                MessageBox.Show("Arquivo salvo com sucesso!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private List<Objeto> ObterObjetos()
+        {
+            try
+            {
+                List<Objeto> objetos = new List<Objeto>();
+                foreach (var item in lstObjetos.Items)
+                {
+                    objetos.Add((Objeto)item);
+                }
+                return objetos;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void lstObjetos_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Delete && lstObjetos.SelectedIndex > -1)
+                {
+                    RemoverDaLista((Objeto)lstObjetos.SelectedItem);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void RemoverDaLista(Objeto objeto)
+        {
+            try
+            {
+                lstObjetos.Items.Remove(objeto);
+                AtualizarTotal();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void lstObjetos_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (lstObjetos.SelectedIndex > -1)
+                {
+                    Objeto objeto = (Objeto)lstObjetos.SelectedItem;
+                    PreencherCampos(objeto);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void PreencherCampos(Objeto objeto)
+        {
+            if (objeto != null)
+            {
+                txtValor.Text = objeto.Valor;
+                txtDescricao.Text = objeto.Descricao;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Limpar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstObjetos.SelectedIndex > -1)
+                {
+                    Objeto objetoOld = (Objeto)lstObjetos.SelectedItem;
+                    Objeto objetoNew = Validar();
+
+                    Alterar(objetoOld,objetoNew);
+                    Limpar();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Alterar(Objeto objetoOld, Objeto objetoNew)
+        {
+            try
+            {
+                RemoverDaLista(objetoOld);
+                IncluirNaLista(objetoNew);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Deseja salvar os dados?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    GravarArquivo();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                e.Cancel = true;
+            }
+        }
+
+        private void labelValor_DoubleClick(object sender, EventArgs e)
+        {
+            txtValor.Visible = !txtValor.Visible;
+        }
+    }
+}
